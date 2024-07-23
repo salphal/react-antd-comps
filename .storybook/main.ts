1937263c40d1254ea6201bd637d7d2e439f85d21
chen <92xiaodao@gmail.com>
2024-07-23 13:59:33 +0800

    feat: ✨ upgrade storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,17 +3,26 @@ import { withoutVitePlugins } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
   stories: ['../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-actions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    '@chromatic-com/storybook'
+  ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
   viteFinal: async (config) => ({
     ...config,
     plugins: await withoutVitePlugins(config.plugins, ['vite:dts']), // skip dts plugin
   }),
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
