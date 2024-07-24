98ab4004dcaa31ca397a2107fce6516d813833e6
chen <92xiaodao@gmail.com>
2024-07-24 15:21:38 +0800

    fix: 🐛 update jest test base table

--- lib/base-table/base-table.test.tsx
+++ lib/base-table/base-table.test.tsx
@@ -5,7 +5,7 @@ import BaseTable from './base-table.tsx';
 
 it('should render a button', () => {
   // arrange
-  render(<BaseTable>Click Me!</BaseTable>);
+  render(<BaseTable />);
 
   // assert
   expect(screen.getByRole('button', { name: 'Click Me!' })).toBeInTheDocument();
@@ -14,7 +14,7 @@ it('should render a button', () => {
 it('should call onClick when clicked', async () => {
   // arrange
   const onClick = vi.fn();
-  render(<BaseTable>Click Me!</BaseTable>);
+  render(<BaseTable />);
 
   // action
   await userEvent.click(screen.getByRole('button', { name: 'Click Me!' }));
