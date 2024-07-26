b58e5ec709f7aeaef32c06922a050c4fae98da1a
chen <92xiaodao@gmail.com>
2024-07-26 11:34:52 +0800

    feat: ✨ update example styles

--- lib/example/example.tsx
+++ lib/example/example.tsx
@@ -52,23 +52,65 @@ const Example: ForwardRefRenderFunction<ExampleRef, ExampleProps> = (
 
   return (
     <React.Fragment>
-      <div className={classNames(['h-100'])}>
-        <h2>Example</h2>
+      <div className={classNames(['h-100', 'p-3'])}>
+        <div className={'text-xl mb-3'}>Example</div>
         <div>
-          <div>string: {String(props.string)}</div>
-          <div>number: {String(props.number)}</div>
-          <div>boolean: {String(props.boolean)}</div>
-          <div>object: {String(JSON.stringify(props.object))}</div>
-          <div>select: {String(props.select)}</div>
-          <div>multi-select: {String(JSON.stringify(props.multiSelect))}</div>
-          <div>radio: {String(props.radio)}</div>
-          <div>inline-radio: {String(props.inlineRadio)}</div>
-          <div>check: {String(JSON.stringify(props.check))}</div>
-          <div>inlineCheck: {String(JSON.stringify(props.inlineCheck))}</div>
-          <div>range: {String(props.range)}</div>
-          <div>file: {String(JSON.stringify(props.file))}</div>
-          <div>color: {String(props.color)}</div>
-          <div>date: {String(props.date)}</div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>string:</div>
+            <div>{String(props.string)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>number:</div>
+            <div>{String(props.number)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>boolean:</div>
+            <div>{String(props.boolean)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>object:</div>
+            <div>{String(JSON.stringify(props.object))}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>select:</div>
+            <div>{String(props.select)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>multi-select:</div>
+            <div>{String(JSON.stringify(props.multiSelect))}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>radio:</div>
+            <div>{String(props.radio)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>inline-radio:</div>
+            <div>{String(props.inlineRadio)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>check:</div>
+            <div>{String(JSON.stringify(props.check))}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>inlineCheck:</div>
+            <div>{String(JSON.stringify(props.inlineCheck))}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>range:</div>
+            <div>{String(props.range)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>file:</div>
+            <div>{String(JSON.stringify(props.file))}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>color:</div>
+            <div>{String(props.color)}</div>
+          </div>
+          <div className={'flex flex-row flex-nowrap pl-3'}>
+            -&nbsp;<div className={'w-32 mb-2'}>date:</div>
+            <div>{String(props.date)}</div>
+          </div>
         </div>
       </div>
     </React.Fragment>
